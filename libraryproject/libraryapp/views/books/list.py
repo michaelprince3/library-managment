9170c7f8e453568a544322cea7bea1134fae5d32
michaelprince3 <michaelprince3@gmail.com>
2020-05-05 07:53:37 -0500

    chapter 3 complete

---
+++ libraryproject/libraryapp/views/books/list.py
@@ -0,0 +1,46 @@
+import sqlite3
+from django.shortcuts import render
+from libraryapp.models import Book
+from libraryapp.models import model_factory
+from ..connection import Connection
+
+
+def book_list(request):
+    if request.method == 'GET':
+        with sqlite3.connect(Connection.db_path) as conn:
+
+            conn.row_factory = model_factory(Book)
+
+            db_cursor = conn.cursor()
+            db_cursor.execute("""
+            select
+                b.id,
+                b.title,
+                b.isbn,
+                b.author,
+                b.yearPublished,
+                b.publisher,
+                b.library_id
+            from libraryapp_book b
+            """)
+
+            all_books = db_cursor.fetchall()
+
+            # for row in dataset:
+            #     book = Book()
+            #     book.id = row['id']
+            #     book.title = row['title']
+            #     book.isbn = row['isbn']
+            #     book.author = row['author']
+            #     book.year_published = row['yearPublished']
+            #     book.librarian_id = row['publisher']
+            #     book.location_id = row['library_id']
+
+                # all_books.append(book)
+
+        template = 'books/list.html'
+        context = {
+            'all_books': all_books
+        }
+
+        return render(request, template, context)
---
+++ libraryproject/libraryapp/views/connection.py
@@ -0,0 +1,2 @@
+class Connection:
+    db_path = "/home/michaelprince3/workspace/python/library-managment/libraryproject/db.sqlite3"
