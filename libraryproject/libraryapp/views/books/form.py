a41a7cf62e231537128718b338c251c0c629f624
michaelprince3 <michaelprince3@gmail.com>
2020-05-06 10:14:48 -0500

    chapter 8 complete

--- libraryproject/libraryapp/views/books/form.py
+++ libraryproject/libraryapp/views/books/form.py
@@ -1,6 +1,7 @@
 import sqlite3
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from ..books.details import get_book
 from libraryapp.models import Book
 from libraryapp.models import Library
 from libraryapp.models import model_factory
@@ -22,12 +23,29 @@ def get_libraries():
 
         return db_cursor.fetchall()
 
+
 @login_required
 def book_form(request):
     if request.method == 'GET':
         libraries = get_libraries()
         template = 'books/form.html'
         context = {
+            'all_libraries': libraries
+        }
+
+        return render(request, template, context)
+
+
+@login_required
+def book_edit_form(request, book_id):
+
+    if request.method == 'GET':
+        book = get_book(book_id)
+        libraries = get_libraries()
+
+        template = 'books/form.html'
+        context = {
+            'book': book,
             'all_libraries': libraries
         }
 
